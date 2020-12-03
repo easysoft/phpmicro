09aa7b9c36decb85a914b36d4e0c3dead3205a01
dixyes <dixyes@gmail.com>
2020-12-03 16:04:15 +0800

    fix rc build without ID name

--- php_micro.h
+++ php_micro.h
@@ -1,8 +1,9 @@
 #ifndef _PHP_MICRO_H
 #define _PHP_MICRO_H
 
+#define PHP_MICRO_SFX_FILESIZE_ID 12345
+
 #ifdef PHP_WIN32
-# define PHP_MICRO_SFX_FILESIZE_ID 12345
 # define MICRO_SFX_EXPORT __declspec(dllexport) __declspec(noinline)
 #else
 # define MICRO_SFX_EXPORT __attribute__((visibility ("default")))
