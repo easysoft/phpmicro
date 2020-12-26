d17a8e2f7cff30cd46ea6de8ac7e9fc4fdac8715
dixyes <dixyes@gmail.com>
2020-12-26 15:55:00 +0800

    add copyright notes

--- php_micro.c
+++ php_micro.c
@@ -1,3 +1,41 @@
+/*
+micro SAPI for PHP - php_micro.c
+main file for micro sapi
+
+Copyright 2020 Longyan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+this file contains source from php
+here's original copyright notice
+   +----------------------------------------------------------------------+
+   | Copyright (c) The PHP Group                                          |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Edin Kadribasic <edink@php.net>                              |
+   |         Marcus Boerger <helly@php.net>                               |
+   |         Johannes Schlueter <johannes@php.net>                        |
+   |         Parts based on CGI SAPI Module by                            |
+   |         Rasmus Lerdorf, Stig Bakken and Zeev Suraski                 |
+   +----------------------------------------------------------------------+
+*/
 
 
 #include "php.h"
@@ -305,6 +343,7 @@ static void sapi_micro_ini_defaults(HashTable *configuration_hash)
 	zval tmp;
 	INI_DEFAULT("report_zend_debug", "0");
 	INI_DEFAULT("display_errors", "1");
+	// TODO: use macro to determine this
 	INI_DEFAULT("ffi.enable", "1");
     // debug only
     INI_DEFAULT("phar.readonly", "0");
